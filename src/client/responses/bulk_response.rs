e127c77a10a41e4240db1c85d395600d42d155f5
Xavier Lange <xrlange@gmail.com>
2019-01-14 14:39:45 -0400

    licensure for copied code

--- src/client/responses/bulk_response.rs
+++ src/client/responses/bulk_response.rs
@@ -2,6 +2,21 @@
 Response types for a [bulk request](https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-bulk.html).
 */
 
+/*
+   Copyright 2019 Ashley Mannix
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+*/
+
 #![allow(unused_variables, dead_code)]
 
 use serde::de::{Deserialize, Deserializer, Error as DeError, MapAccess, SeqAccess, Visitor};
